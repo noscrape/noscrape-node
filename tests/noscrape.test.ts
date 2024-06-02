600a4bed8c5d7d6d5ab6cd882125397b76f44250
Bernhard Schönberger <bernhard.schoenberger@leebmann.de>
2024-06-02 21:22:42 +0200

    update test

--- tests/noscrape.test.ts
+++ tests/noscrape.test.ts
@@ -1,26 +1,36 @@
-import {expect} from "@jest/globals";
-import {Noscrape} from "../src/noscrape";
+import { expect } from "@jest/globals";
+import { Noscrape } from "../src/noscrape";
 
-const fontFile = __dirname + '/../example/example.ttf'
+const fontFile = __dirname + "/../example/example.ttf";
 
 describe("test noscrape", () => {
-    test("benchmark", () => {
-        expect(true).toBeTruthy()
-    })
+  test("benchmark", () => {
+    expect(true).toBeTruthy();
+  });
 
+  test("obfuscate", () => {
+    const noscrape = new Noscrape(fontFile);
+    const obfuscatedString = noscrape.obfuscate("test");
+    expect(obfuscatedString).toHaveLength(4);
 
-    test("obfuscate", () => {
-        const noscrape = new Noscrape(fontFile);
-        const obfuscated = noscrape.obfuscate("test");
-        expect(obfuscated).toHaveLength(4)
-    })
+    const obfuscatedNumber = noscrape.obfuscate(1234);
+    expect(obfuscatedNumber).toHaveLength(4);
 
-    test("test rendering", async () => {
-        const noscrape = new Noscrape(fontFile);
-        const obfuscated = noscrape.obfuscate("test");
-        expect(obfuscated).toHaveLength(4)
+    const obfuscatedObject = noscrape.obfuscate({
+      test: "string",
+      another: {
+        test: "another string",
+      },
+    });
+    expect(obfuscatedObject.another.test).toHaveLength(14);
+  });
 
-        const r = await noscrape.render();
-        expect(r.toString("base64")).toHaveLength(4720)
-    })
-})
+  test("test rendering", async () => {
+    const noscrape = new Noscrape(fontFile);
+    const obfuscated = noscrape.obfuscate("test");
+    expect(obfuscated).toHaveLength(4);
+
+    const r = await noscrape.render();
+    expect(r.toString("base64")).toHaveLength(4720);
+  });
+});
